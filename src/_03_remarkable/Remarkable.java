580496a816bf2c6e9a2a631542f9b460fd13365d
League Student <league@iMac-17.attlocal.net>
2019-09-08 15:58:33 -0700

    Riddler

--- src/_03_remarkable/Remarkable.java
+++ src/_03_remarkable/Remarkable.java
@@ -8,12 +8,30 @@ public class Remarkable {
 public static void main(String[] args) {
 
 		// 1. Save something remarkable about each person in a variable.
-String Arshia="Kind"
-String
+String Arshia="Super Cool";
+String Ashay="Hard-working";
+String Devin="Positive";
+String Andrew="Helpful";
+String Darren="Kind and Nice";
 		// 2. Ask the user to enter a name. Store their answer in a variable.
 String input= JOptionPane.showInputDialog(null, "Enter a name");
-		// 3. In a pop-up, tell the user what is remarkable about that person. 
+if(input.equalsIgnoreCase("Arshia")) {
+JOptionPane.showMessageDialog(null, Arshia);
 
+}else if( input.equalsIgnoreCase("Ashay")){
+	JOptionPane.showMessageDialog(null, Ashay);
+	if(input.equalsIgnoreCase("Devin")) {
+		JOptionPane.showMessageDialog(null, Devin);
+		if(input.equalsIgnoreCase("Andrew")) {
+			JOptionPane.showMessageDialog(null, Andrew);
+			if(input.equalsIgnoreCase("Darren")) {
+				JOptionPane.showMessageDialog(null, Darren);
+		
+
+// 3. In a pop-up, tell the user what is remarkable about that person. 
+}
 	}
 }
-
+}
+}
+}
--- src/_05_tasty_tomato/tasty_tomato/tasty_tomato.pde
+++ src/_05_tasty_tomato/tasty_tomato/tasty_tomato.pde
@@ -4,7 +4,15 @@ void setup() {
 void draw() {
     background(200, 200, 200);
     noStroke();
+  fill(#E8465C);
     ellipse(150, 200, 150, 150);
     ellipse(212, 200, 150, 150);
+   fill(#4BF057);
     rect(176, 103, 12, 32);
+    if(mousePressed){
+      fill(#EAD8AF);
+   ellipse(225, 225, 50, 50);
+
+    
+}
 }
