484cec4288acf5ca2d985fdd17de533d025a6016
League Student <league@iMac-17.attlocal.net>
2019-09-15 15:56:14 -0700

    Zombie Eyes

--- src/_07_riddler/TheRiddler.java
+++ src/_07_riddler/TheRiddler.java
@@ -11,18 +11,30 @@ public class TheRiddler {
 
 	public static void main(String[] args) {
 
-		// 1. Make a variable to hold the score
-String score
+				// 1. Make a variable to hold the score
+int score = 0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-JOptionPane.showInputDialog(null, "")
+ String question1 = JOptionPane.showInputDialog(null, "Which word in the dictionary is spelled incorrectly?");
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
+if (question1.equalsIgnoreCase("incorrectly")) {
+JOptionPane.showMessageDialog(null, "Correct!");
+score ++;
 		// 5. Otherwise, say "wrong" and tell them the answer
-
-		// 6. Add some more riddles
-
-		// 2. Make a pop up to show the score.
+	} else {
+		JOptionPane.showMessageDialog(null, "Wrong. The answer is incorrectly");
 		
+		// 6. Add some more riddles
+String question2 = JOptionPane.showInputDialog(null, "What gets broken without being held");
+if (question2.equalsIgnoreCase("A promise")) {
+JOptionPane.showMessageDialog(null, "Correct!");
+score +=1;
+}else {
+	JOptionPane.showMessageDialog(null, "Wrong. The answer is a promise");
+		// 2. Make a pop up to show the score.
+	JOptionPane.showMessageDialog(null, score);
+	
+}			
 	}
 }
 
+}
--- src/_09_zombie_eyes/zombie_eyes/zombie_eyes.pde
+++ src/_09_zombie_eyes/zombie_eyes/zombie_eyes.pde
@@ -1,7 +1,9 @@
 
 void setup() {
-
+  size(900, 900);
+PImage face = loadImage("photo2.jpg");
+image(face, 0, 0);
 }
 void draw() {
-
+ellipse(224, 122, 30, 30);
 }
